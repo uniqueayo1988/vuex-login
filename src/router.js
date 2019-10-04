fca1c49909752714caa5a0fc8dc605d65c85fc0f
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-10-04 19:37:51 +0100

    Ch(): Add dependencies - Install dependencies

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import WelcomePage from './components/welcome/welcome.vue'
+import DashboardPage from './components/dashboard/dashboard.vue'
+import SignupPage from './components/auth/signup.vue'
+import SigninPage from './components/auth/signin.vue'
 
 Vue.use(Router)
 
@@ -10,16 +13,19 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      component: WelcomePage
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/signup',
+      component: SignupPage
+    },
+    {
+      path: '/signin',
+      component: SigninPage
+    },
+    {
+      path: '/dashboard',
+      component: DashboardPage
     }
   ]
 })
